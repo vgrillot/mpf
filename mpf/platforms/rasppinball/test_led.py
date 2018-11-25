311424465828db8ae8ca8f648f07374c4ba2c8b4
vgrillot <grivin@gmail.com>
2018-11-25 12:55:05 +0000

    fix led color

--- mpf/platforms/rasppinball/test_led.py
+++ mpf/platforms/rasppinball/test_led.py
@@ -21,13 +21,22 @@ def test_led():
     strip = Adafruit_NeoPixel(64, 10, 800000, 5, False, 255)
     strip.begin()
     for i in range(0, 50):
-        strip.setPixelColorRGB(0, 0, 0, 0)
+        print("red")
+        strip.setPixelColorRGB(0, 0, 0xFF, 0)   # red 
         strip.show()
-        #sleep(5)
-        strip.setPixelColorRGB(0, 0, 0xFF, 0)
+        #time.sleep(1)
+
+        print("green")
+        strip.setPixelColorRGB(0, 0xFF, 0, 0)   # green
         strip.show()
-        #sleep(5)
-    strip.setPixelColorRGB(0, 0xFF, 0, 0)
+        #time.sleep(1)
+
+        print("blue")
+        strip.setPixelColorRGB(0, 0, 0, 0xFF)   # blue
+        strip.show()
+        #time.sleep(1)
+
+    strip.setPixelColorRGB(0, 0, 0xFF, 0xFF)
     strip.show()
 
 if __name__ == "__main__":
